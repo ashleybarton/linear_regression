e74b035f55aae6b9e6cc09caaafcf8b829f086aa
Ashley Barton <barton.ash@gmail.com>
2015-05-03 07:04:58 -0700

    initial commit

---
+++ CalculateMeans.java
@@ -0,0 +1,30 @@
+// Ashley Barton
+//
+// Introductary Linear Regression
+//
+// b = y_ - m*x_
+//
+// m = (x_ y_ - xy_) / (x_)^2 - x^2_
+//
+// To find our best fit line,
+// we need to calculate the slope (m)
+// as well as our mean values
+//
+
+public class CalculateMeans {
+
+    public double calculate_x_mean(double[] x_values){
+        
+    }
+
+    public double calculate_y_mean(double[] y values){
+        
+    }
+    
+    public double calculate_xy_mean(double[] y values){
+        
+    }
+
+    public double find_slope(double xyMean, double xMean, double yMean){
+    }
+}
